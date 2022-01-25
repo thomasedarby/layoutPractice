4cc7fdb6b7e237db5324a852fb3ef5dd38b1e45e
thomasedarby <thomasedarby@live.co.uk>
2022-01-25 23:59:22 +0000

    ADid a lot of the starter work. Sorted out the layout of the top section, created separate file for the icon css and added the background to profile image as well as inputted the text from my cv

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hello World')
+var i = 0
+var txt = "Digital Curriculum Vitae" /* The text */
+var speed = 80 /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+	if (i < txt.length) {
+		document.getElementById("tagline").innerHTML += txt.charAt(i)
+		i++
+		setTimeout(typeWriter, speed)
+	}
+}
+
+typeWriter()
